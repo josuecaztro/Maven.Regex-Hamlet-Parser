7e2af336987f1ae2a8c5881070143fb2ea7ee37c
Josue <josue@Zipcoders-MacBook-Pro-75.local>
2024-08-07 11:41:36 -0400

    Copy to New File Works

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,5 +1,4 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 /**
@@ -35,5 +34,63 @@ public class HamletParser {
     public String getHamletData(){
         return hamletData;
     }
+
+    public void createNewFile(){
+        //creates a new file
+        try {
+            File myObj = new File("src/main/resources/newHamlet.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public void modifyToNewFile(File a, File b) throws IOException {
+        FileInputStream in = new FileInputStream(a);
+        FileOutputStream out = new FileOutputStream(b);
+
+        try {
+
+            int n;
+
+            // read() function to read the
+            // byte of data
+            while ((n = in.read()) != -1) {
+                // write() function to write
+                // the byte of data
+                out.write(n);
+            }
+        }
+        finally {
+            if (in != null) {
+
+                // close() function to close the
+                // stream
+                in.close();
+            }
+            // close() function to close
+            // the stream
+            if (out != null) {
+                out.close();
+            }
+        }
+        System.out.println("File Copied");
+    }
+
+    public static void main(String[] args) throws IOException {
+//        HamletParser hp = new HamletParser();
+//        File fileOne = new File("src/main/resources/hamlet.txt");
+//        File fileTwo = new File("src/main/resources/hamletCopy.txt");
+//
+//        hp.modifyToNewFile(fileOne, fileTwo);
+
+
+    }
+
 
 }
