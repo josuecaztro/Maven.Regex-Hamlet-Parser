806400948fa345958d8f37775cd746b7e268bbd3
Josue <josue@Zipcoders-MacBook-Pro-75.local>
2024-08-07 10:32:53 -0400

    startedTests

--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,5 +1,9 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -15,10 +19,31 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        String sentence = "Hamlet is in the building!";
+        String toChange = "Hamlet";
+        String expected = "Leon is in the building!";
+
+        Pattern pattern = Pattern.compile(toChange);
+        Matcher matcher = pattern.matcher(sentence);
+
+        String actual = matcher.replaceAll("Leon");
+
+        Assert.assertEquals(expected,actual);
+
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        String sentence = "Horatio is in the building!";
+        String toChange = "Horatio";
+        String expected = "Tariq is in the building!";
+
+        Pattern pattern = Pattern.compile(toChange);
+        Matcher matcher = pattern.matcher(sentence);
+
+        String actual = matcher.replaceAll("Tariq");
+
+        Assert.assertEquals(expected,actual);
     }
 
     @Test
